a07fc500b6f008484393b3c66515df737eec80f6
Leenclair <leesu9317@gmail.com>
2024-03-12 22:43:00 +0900

    jpa-basic practical proj, jpa-shop commit

---
+++ src/main/java/jpabook/jpashopbasic/domain/Delivery.java
@@ -0,0 +1,19 @@
+package jpabook.jpashopbasic.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Delivery extends BaseEntity {
+
+    @Id @GeneratedValue
+//    @Column(name = "DELIVERY_ID")
+    private Long id;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+    private DeliveryStatus status;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+}
